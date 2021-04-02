b4bd2db2fbc7823714de771fc2efcf354e636309
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-04-02 16:55:04 -0500

    alerts tied to generate button to gather values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 // Assignment code here
 
-// Generator Functions
+
+
+
+// Tie my functions in to an object
+
+const randomFunction = {
+  lower: generateRandomLower,
+  upper: generateRandomUpper,
+  special: generateRandomSpecial,
+  number: generateRandomNumber
+};
+
+console.log(randomFunction);
+
+// Generator Functions from https://www.w3schools.com/html/html_charset.asp
 
 function generateRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
@@ -18,17 +32,28 @@ function generateRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
 
-console.log(generateRandomLower());
-console.log(generateRandomUpper());
-console.log(generateRandomSpecial());
-console.log(generateRandomNumber());
 
 
-// Get references to the #generate element
+// Get references to the #generate element (DOM element generate password button)
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var passwordLength = parseInt(window.prompt("enter length of password. Must be 8-128 in length"));  
+  var hasLower = window.confirm("Would you like to include lower case in your password?");  
+  var hasUpper = window.confirm("Would you like to include Upper case in your password?");
+  var hasSpecial = window.confirm("Would you like to include Special characters in your password?");
+  var hasNumber = window.confirm("Would you like to include numbers in your password?");
+  console.log(passwordLength);
+  console.log(hasLower);
+  console.log(hasUpper);
+  console.log(hasSpecial);
+  console.log(hasNumber);
+
+  
+
+  
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -38,3 +63,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
