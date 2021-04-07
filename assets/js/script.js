8d63b79be0a75c5cf798f9ea3793e451871d0ccf
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-04-07 01:15:52 -0500

    cleaned up the funtions. started on the gen func

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,14 @@
 // Assignment code here
 
 
-// Password generator
-
-
-
-
-
-
-// Tie my functions in to an object
+// Tie my generator functions in to an object
 
 const randomFunction = {
-  lower: generateRandomLower,
-  upper: generateRandomUpper,
-  special: generateRandomSpecial,
-  number: generateRandomNumber
+  lower: generateRandomLower(),
+  upper: generateRandomUpper(),
+  special: generateRandomSpecial(),
+  number: generateRandomNumber()
 };
-
 
 // Generator Functions from https://www.w3schools.com/html/html_charset.asp
 
@@ -38,25 +30,65 @@ function generateRandomNumber() {
 
 
 
+
+
+// Get Password criteria from user input
+
+function generatePasswordOptions(){
+  const passwordLength = parseInt(window.prompt("enter length of password. Must be 8-128 in length"));
+    if (passwordLength < 8) {
+      alert('must be at least 8 characters long')
+      generatePasswordOptions()  }
+  const includeLower = window.confirm("Would you like to include lower case in your password?");  
+  const includeUpper = window.confirm("Would you like to include Upper case in your password?");
+  const includeSpecial = window.confirm("Would you like to include Special characters in your password?");
+  const includeNumber = window.confirm("Would you like to include numbers in your password?");  
+  passwordOptions = {
+    haslower: includeLower,
+    hasUpper: includeUpper,
+    hasSpecial: includeSpecial,
+    hasNumber: includeNumber,
+    length: passwordLength
+  };
+  return passwordOptions;
+}
+
+
+// Generate Password with passwordOption and randomFunction numbers
+function generatePassword() {  
+  const passwordOptions = generatePasswordOptions();
+    console.log('passwordOptions', typeof passwordOptions)
+    console.log(passwordOptions)
+  var randomfunc = new Object(randomFunction)
+    console.log('randomFunction', typeof randomFunction)
+    console.log(randomFunction)
+  let generatedPassword = 'hello';
+
+
+
+
+
+
+  const finalPassword = generatedPassword
+    console.log(finalPassword)
+  return finalPassword;
+}
+
+
+
 // Get references to the #generate element (DOM element generate password button)
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  const length = parseInt(window.prompt("enter length of password. Must be 8-128 in length"));
-  const includeLower = window.confirm("Would you like to include lower case in your password?");  
-  const includeUpper = window.confirm("Would you like to include Upper case in your password?");
-  const includeSpecial = window.confirm("Would you like to include Special characters in your password?");
-  const includeNumber = window.confirm("Would you like to include numbers in your password?");
- 
-  
-
+function writePassword() {  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); {
-}
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
