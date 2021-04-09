1eedb28983a2278665818182e3928c915364db35
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-04-08 19:34:35 -0500

    updated generate password button to generate random functions instead of app start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,8 @@
 // Assignment code here
 
 
-// Tie my generator functions in to an object
-const randomFunction = {
-  lower: generateRandomLower,
-  upper: generateRandomUpper,
-  special: generateRandomSpecial,
-  number: generateRandomNumber
-};
 
-
-// Generator Functions charset from https://www.w3schools.com/html/html_charset.asp
-function generateRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-}
-function generateRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-}
-function generateRandomSpecial() {
-  return String.fromCharCode(Math.floor(Math.random() * 15) + 33)
-}
-function generateRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
-}
-
-
-// Get Password criteria from user input
+// Get Password criteria from user input (prompts)
 function generatePasswordOptions(){
   const passwordLength = parseInt(window.prompt("Enter the length of password you would like. Must be 8-128 in length"));
     if (passwordLength < 8 || passwordLength > 128 ) {
@@ -43,15 +20,40 @@ function generatePasswordOptions(){
     hasNumber: includeNumber,
     passwordLength: passwordLength
   };
+  //return the password option in object form
   return passwordOptions;
 }
 
 
 // Generate Password with passwordOption and randomFunction numbers
 function generatePassword() {  
+
+  // Tie my generator functions in to an object
+  const randomFunction = {
+    lower: generateRandomLower,
+    upper: generateRandomUpper,
+    special: generateRandomSpecial,
+    number: generateRandomNumber
+  };
+
+  // Generator Functions charset from https://www.w3schools.com/html/html_charset.asp
+  function generateRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+  }
+  function generateRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+  }
+  function generateRandomSpecial() {
+    return String.fromCharCode(Math.floor(Math.random() * 15) + 33) + String.fromCharCode(Math.floor(Math.random() * 7) + 58)
+  }
+  function generateRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+  }
+
   // password generated based on variables
-  let generatedPassword = '';
+  let generatedPassword = ''
   // retrieve passwordOptions object
+
   var passwordOptions = generatePasswordOptions()  
   // change object names to match with arrays
     var lower = passwordOptions.hasLower
@@ -66,9 +68,9 @@ function generatePassword() {
   // make passwordOptions object into an array with true/false arrays built in each array
   const passwordOptionsArray = [{lower}, {upper}, {special}, {number}].filter(item => Object.values(item)[0])
   // start over if user hits all cancels
-  if(passwordRequirements === 0){
-      return '';
-    }
+    if(passwordRequirements === 0){
+        return '';
+      }
   
   // forLoop to build the password with all requirments
   for(let i=0; i<passwordLength; i+=passwordRequirements) {
@@ -84,6 +86,7 @@ function generatePassword() {
 }
 
 
+// Provided Code -
 // Get references to the #generate element (DOM element generate password button)
 var generateBtn = document.querySelector("#generate");
 
@@ -95,8 +98,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+generateBtn.addEventListener("click", writePassword)
 
 
 
